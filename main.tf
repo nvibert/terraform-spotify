6a1720ac69659b8df364c89bad0c90f30e5c4098
nvibert <nico.vibert@hashicorp.com>
2021-09-13 12:25:50 +0100

    First commit to repo

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.1.5"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+variable "spotify_api_key" {
+  type = string
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+resource "spotify_playlist" "playlist" {
+  name        = "Terraform Summer Playlist"
+  description = "This playlist was created by Terraform"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[2].id,
+  ]
+}
+
+data "spotify_search_track" "by_artist" {
+  artists = ["The Weeknd"]
+  #  album = "Starboy"
+  #  name  = "Starboy"
+}
+
+output "tracks" {
+  value = data.spotify_search_track.by_artist.tracks
+}
